d2367f3970d89dec4734c28ec00db05aa3bb6465
Mike Zuev <zuevmihail128@gmail.com>
2022-05-22 05:29:37 -0700

    add a new class the best

--- src/main/java/lesson2/HomeWork2.java
+++ src/main/java/lesson2/HomeWork2.java
@@ -2,6 +2,10 @@ package lesson2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        System.out.println("");
+
+        System.out.println("flsdjlfsdk");
+    }
+    public static boolean numbersChecker(){
+        return true;
     }
 }
